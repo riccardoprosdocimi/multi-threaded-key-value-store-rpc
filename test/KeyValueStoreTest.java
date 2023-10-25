f0ad5c1d8ebf0a6a78a1dff7f6ec9e5604f40fa9
Riccardo Prosdocimi <95254927+riccardoprosdocimi@users.noreply.github.com>
2023-10-25 12:26:45 -0400

    Fix bug

--- test/KeyValueStoreTest.java
+++ test/KeyValueStoreTest.java
@@ -1,26 +1,43 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import server.IServer;
+import server.Server;
 
-// The code below uses 100 threads to manipulate the hashmap and identify if there is an issue with the thread-safety of the RMI server.
+/**
+ * This is a Junit test class for the RMI server.
+ */
 public class KeyValueStoreTest {
+  private IServer obj;
+
+  /**
+   * Instantiates and run the RMI server at port 1099.
+   */
+  @BeforeEach
+  void setUp() {
+    try {
+      this.obj = new Server(1099);
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Tests the thread safety of the RMI server by using 100 threads to manipulate the hashmap.
+   *
+   * @throws InterruptedException the interrupted exception
+   */
   @Test
-  public void testThreadSafety() throws InterruptedException, RemoteException, NotBoundException, MalformedURLException {
+  public void testThreadSafety() throws InterruptedException {
     int numThreads = 100;
     CountDownLatch addLatch = new CountDownLatch(numThreads / 2);
     CountDownLatch deleteLatch = new CountDownLatch(numThreads / 2);
     AtomicInteger adds = new AtomicInteger(0);
     AtomicInteger deletes = new AtomicInteger(0);
-    IServer obj = (IServer) Naming.lookup("//localhost/TranslationServer");
 
     Runnable addTask = () -> {
       try {
@@ -29,23 +46,23 @@ public class KeyValueStoreTest {
         System.err.println(e.getMessage());
         e.printStackTrace();
       } finally {
-        addLatch.countDown();
+        addLatch.countDown(); // start the add operations countdown
       }
     };
 
     Runnable deleteTask = () -> {
       try {
-        addLatch.await();
+        addLatch.await(); // wait for the add operations to finish
         obj.delete("key" + deletes.incrementAndGet());
       } catch (Exception e) {
         System.err.println(e.getMessage());
         e.printStackTrace();
       } finally {
-        deleteLatch.countDown();
+        deleteLatch.countDown(); // start the delete operations countdown
       }
     };
 
-    for (int i = 0; i < numThreads; i++) {
+    for (int i = 0; i < numThreads; i++) { // spun the threads
       if (i % 2 == 0) {
         new Thread(addTask).start();
       } else {
@@ -53,12 +70,25 @@ public class KeyValueStoreTest {
       }
     }
 
-    deleteLatch.await();
+    deleteLatch.await(); // wait for the delete operations to finish
 
     try {
-      int expectedSize = adds.get() - deletes.get();
-      int actualSize = obj.getMapSize();
-      assertEquals(expectedSize, actualSize);
+      int expectedSize = adds.get() - deletes.get(); // compute the expected hashmap size
+      int actualSize = obj.getMapSize(); // get the actual hashmap size
+      assertEquals(expectedSize, actualSize); // compare the two values
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Shuts the RMI server down gracefully.
+   */
+  @AfterEach
+  void tearDown() {
+    try {
+      obj.shutdown();
     } catch (Exception e) {
       System.err.println(e.getMessage());
       e.printStackTrace();
