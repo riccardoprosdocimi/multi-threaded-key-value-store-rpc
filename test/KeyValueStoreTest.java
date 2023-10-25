fccd8a46e4cbe051814b31f18d069df1a21300a7
Riccardo Prosdocimi <95254927+riccardoprosdocimi@users.noreply.github.com>
2023-10-24 23:45:59 -0400

    Bug in the JUnit test: Naming.lookup throws a NotBoundException

--- test/KeyValueStoreTest.java
+++ test/KeyValueStoreTest.java
@@ -1,6 +1,11 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,28 +14,35 @@ import server.IServer;
 // The code below uses 100 threads to manipulate the hashmap and identify if there is an issue with the thread-safety of the RMI server.
 public class KeyValueStoreTest {
   @Test
-  public void testThreadSafety() throws InterruptedException {
+  public void testThreadSafety() throws InterruptedException, RemoteException, NotBoundException, MalformedURLException {
     int numThreads = 100;
-    CountDownLatch latch = new CountDownLatch(numThreads);
+    CountDownLatch addLatch = new CountDownLatch(numThreads / 2);
+    CountDownLatch deleteLatch = new CountDownLatch(numThreads / 2);
     AtomicInteger adds = new AtomicInteger(0);
     AtomicInteger deletes = new AtomicInteger(0);
+    IServer obj = (IServer) Naming.lookup("//localhost/TranslationServer");
+
     Runnable addTask = () -> {
       try {
-        IServer obj = (IServer) Naming.lookup("//localhost/TranslationServer");
         obj.put("key" + adds.incrementAndGet(), "value");
       } catch (Exception e) {
-     
+        System.err.println(e.getMessage());
+        e.printStackTrace();
+      } finally {
+        addLatch.countDown();
       }
-      latch.countDown();
     };
+
     Runnable deleteTask = () -> {
       try {
-        IServer obj = (IServer) Naming.lookup("//localhost/TranslationServer");
+        addLatch.await();
         obj.delete("key" + deletes.incrementAndGet());
       } catch (Exception e) {
-  
+        System.err.println(e.getMessage());
+        e.printStackTrace();
+      } finally {
+        deleteLatch.countDown();
       }
-      latch.countDown();
     };
 
     for (int i = 0; i < numThreads; i++) {
@@ -40,14 +52,16 @@ public class KeyValueStoreTest {
         new Thread(deleteTask).start();
       }
     }
-    latch.await();
+
+    deleteLatch.await();
+
     try {
-      IServer obj = (IServer) Naming.lookup("//localhost/TranslationServer");
       int expectedSize = adds.get() - deletes.get();
       int actualSize = obj.getMapSize();
       assertEquals(expectedSize, actualSize);
     } catch (Exception e) {
-   
+      System.err.println(e.getMessage());
+      e.printStackTrace();
     }
   }
 }
